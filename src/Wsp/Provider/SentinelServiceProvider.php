4a45c6bc2b80d07b77a6699c45bdbc701caf6040
assarte <be.creative@webapper.hu>
2015-11-01 15:10:55 +0100

    * minor refactoring on SentinelServiceProvider

--- src/Wsp/Provider/SentinelServiceProvider.php
+++ src/Wsp/Provider/SentinelServiceProvider.php
@@ -16,8 +16,19 @@ use Wsp\SentinelBootstrapper;
 class SentinelServiceProvider implements ServiceProviderInterface {
 	public function register(Application $app)
 	{
-		$bootstrapper = new SentinelBootstrapper($app);
-		$app['sentinel'] = $bootstrapper->createSentinel();
+		$app['sentinel'] = static::instance($app);
+	}
+
+	/**
+	 * @param Application $app
+	 * @param SentinelBootstrapper $bootstrapper
+	 * @return \Cartalyst\Sentinel\Sentinel
+	 */
+	public static function instance(Application $app, SentinelBootstrapper $bootstrapper = null) {
+		if ($bootstrapper === null) {
+			$bootstrapper = new SentinelBootstrapper($app);
+		}
+		return $bootstrapper->createSentinel();
 	}
 
 	public function boot(Application $app)
